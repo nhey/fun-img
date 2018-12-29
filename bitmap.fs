859f835eef6f54132185d76e199385cc6caffa39
Nikolaj Hey Hinnerskov <nikolajhey@gmail.com>
2018-12-30 00:29:29 +0100

    gogo

--- bitmap.fs
+++ bitmap.fs
@@ -2,7 +2,14 @@
 * http://www.fssnip.net/sn/title/Bitmap-Primitives-Helpers
 * & Martin Elsmans img_util library
 *)
+module BitmapUtil
 open System.Drawing
+
+(* colors *)
+let fromArgb (a:int, r:int, g:int, b:int) : Color =
+  Color.FromArgb(a,r,g,b)
+
+(* images *)
 
 let toPngFile (fname : string) (bmp : Bitmap) =
   bmp.Save(fname, Imaging.ImageFormat.Png) |> ignore
@@ -11,7 +18,7 @@ let toPngFile (fname : string) (bmp : Bitmap) =
 // where 'a could be of type Color
 let toBitmap a =
   let height = (a |> Array.map (fun (x,_,_) -> x) |> Array.max) + 1
-  let height = (a |> Array.map (fun (_,y,_) -> x) |> Array.max) + 1
+  let width = (a |> Array.map (fun (_,y,_) -> y) |> Array.max) + 1
   let bmp = new Bitmap(width, height)
   a |> Array.iter (fun (x,y,c) -> bmp.SetPixel(x,y,c))
   bmp
--- img.fs
+++ img.fs
@@ -1,4 +1,9 @@
 type Point = (float, float)
 type Image 'a = Point -> 'a
 type Region = Image Bool
+
+let even x = (x % 2 = 0)
+let checker (x,y) = even(abs(x) + abs(y))
+// nu lav array af (x,y,color) sort/hvid og toPngFile
+
 type PolarPoint = (float, float)
