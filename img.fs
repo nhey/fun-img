2a114c4a4ca03df6548e7654d4d637c5888353e8
Nikolaj Hey Hinnerskov <nikolajhey@gmail.com>
2019-01-23 21:53:54 +0100

    alternating and polar rings

--- img.fs
+++ img.fs
@@ -6,16 +6,32 @@ type Point = float * float
 type 'a Image = Point -> 'a
 type Region = bool Image
 
+// approximate to bool
+let (<<>>) : float -> float -> bool =
+  fun x y -> abs(y-x) < 0.05
+// combine regions
+let (<||>) : Region -> Region -> Region =
+  fun r r' -> fun (x,y) -> r (x,y) || r' (x,y)
+
+// images for drawing
 let even x = (x % 2 = 0)
 let floori x = int (floor x)
 let checker : Region =
-  fun (x,y) -> even (floori (abs x) + floori (abs y))
+  fun (x,y) -> even (floori x + floori y)
 
-let (<<>>) : float -> float -> bool =
-  fun x y -> abs(y-x) < 0.05
+let distO : Point -> float =
+  fun (x,y) -> System.Math.Sqrt (x**2.0 + y**2.0)
+let altRings : Region =
+  fun (x,y) -> even (floori (distO (x,y)))
 
-let (<||>) : Region -> Region -> Region =
-  fun r r' -> fun (x,y) -> r (x,y) || r' (x,y)
+// polar
+type PolarPoint = float * float
+let toPolar : Point -> PolarPoint =
+  fun (x,y) -> (distO (x,y), atan2 y x)
+let polarChecker n : Region =
+  let sc (p, θ) = (p, θ * (float n) / System.Math.PI)
+  checker << sc << toPolar
+
 
 (* konkretisering; fra abstrakt billede til bitmap *)
 let toBitmap (img : Region) scale width height =
@@ -29,5 +45,6 @@ let toBitmap (img : Region) scale width height =
       in BitmapUtil.setPixel c (x,y) bmp
   bmp
 
-toBitmap checker 10.0 600 600
+// toBitmap (altRings <||> checker) 10.0 600 600
+toBitmap (altRings <||> polarChecker 10) 60.0 600 600
 |> toPngFile "test.png"
